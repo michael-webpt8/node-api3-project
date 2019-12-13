100e63c9f7fe41fd4acfe908c2d9b395260b141e
Michael Williams <manicdesigner@gmail.com>
2019-12-12 19:03:50 -0500

    add / server.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // code away!
 const express = require('express');
 const userRouter = require('./users/userRouter');
+const serverRoute = require('./server');
 
 const server = express();
 server.use(express.json());
 
+server.use('/', serverRoute);
 server.use('/users', userRouter);
 
 server.use((req, res) => {
-    res.status(404).json({ message: "Route was not Found" })
-})
+  res.status(404).json({ message: 'Route was not Found' });
+});
 
 server.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ message: "An internal error occured, Please try again later." })
-})
+  console.log(err);
+  res
+    .status(500)
+    .json({ message: 'An internal error occured, Please try again later.' });
+});
 
 server.listen(4000, () => {
-    console.log("\n*** Server running on http://localhost:4000 ***\n")
-})
+  console.log('\n*** Server running on http://localhost:4000 ***\n');
+});
