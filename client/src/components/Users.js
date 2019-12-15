2c2153c69a3f6e49ec90b62ca4e23bd1f5724692
Mikey Williams <manicdesigner@gmail.com>
2019-12-15 10:49:03 -0500

    add Users section

---
+++ client/src/components/Users.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Users = props => {
+
+    return (
+        <>
+            <h2>Users</h2>
+        </>
+    )
+}
+export default Users;
