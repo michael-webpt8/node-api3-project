2952d9a7fe0274696b7846193384dc3f3ecbb9e1
Mikey Williams <manicdesigner@gmail.com>
2019-12-15 10:44:56 -0500

    add bootstrap min css

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Router as BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
 
